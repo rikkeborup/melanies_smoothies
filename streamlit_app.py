a4dee79e046058d3058404103ac2f809b677652d
rikkeborup <72106442+rikkeborup@users.noreply.github.com>
2025-07-04 13:16:15 +0200

    api i dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def insert_and_clear(insert_stmt, name_on_order):
     clear_text()
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
