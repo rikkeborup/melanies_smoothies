8d889ddeedcc7dcbe7307ebe1b7a87ede8f5f174
rikkeborup <72106442+rikkeborup@users.noreply.github.com>
2025-07-04 13:41:12 +0200

    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,6 @@ st.write('The name on your smoothie will be:', name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', 
                                   my_dataframe, 
@@ -53,7 +51,7 @@ if ingredients_list:
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
